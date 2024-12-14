901f796bed02fef5f5b7d4c54007774236cabcd6
Ирина <ch_semen@mail.ru>
2024-12-14 20:23:24 +0300

    p660

---
+++ courses/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework import generics
+from courses.api.serializers import SubjectSerializer
+from django.db.models import Count
+from courses.api.pagination import StandardPagination
+from courses.models import Subject
+
+class SubjectListView(generics.ListAPIView):
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
+    serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
+
+class SubjectDetailView(generics.RetrieveAPIView):
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
+    serializer_class = SubjectSerializer
